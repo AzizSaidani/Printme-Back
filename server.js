13a42f278d3b5a0fb328e98a9ca1d649f7fbe99d
AzizSaidani <Azizsaidani1999@gmail.com>
2025-03-08 16:41:07 +0100

    changing variables to envs

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 // Middleware
 
 const corsOptions = {
-  origin: 'https://print-me-two.vercel.app', // Allow frontend domain
+  origin: ['https://print-me-two.vercel.app', 'http://localhost:4200'],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Allow cookies/auth headers
 };
